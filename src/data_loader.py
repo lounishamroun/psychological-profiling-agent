d842d0d850a78e113a11dd0b07b3c07c8b53d80f
Lounis Hamroun@gmail.com <lounis.hamroun@gmail.com>
2026-03-30 22:42:30 +0200

    feat: Adding dependencies

---
+++ src/data_loader.py
@@ -0,0 +1,4 @@
+from datasets import load_dataset
+
+# Login using e.g. `huggingface-cli login` to access this dataset
+ds = load_dataset("darkknight25/Interrogation_Dataset_for_AI_Deception_Detection")
